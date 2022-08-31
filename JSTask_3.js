fa5d441fc681aab653f4f75b23b11dab713a4434
petruha <petruha.svystun.90@gmail.com>
2022-08-31 17:14:05 +0300

    JSTask_3.js

---
+++ JSTask_3.js
@@ -0,0 +1,26 @@
+let CC = "rrr";
+let age = Number(CC)
+let AAA = typeof(age);
+if (AAA == "number"){    
+        const checkAge = function(age){
+            if (age < 18){
+               console.log ("You don’t have access cause your age is" + " " + age + " " + "It’s less then");
+           }else if (age >=  18 && age < 60){
+               console.log ("Welcome  !");
+           }else if (age  > 60){
+               console.log ("Keep calm and look Culture channel");
+           }else  
+           {console.log ("Technical work");
+           }
+        }
+           checkAge(age);
+        }
+ else{
+    console.log(false);
+ }
+ 
+//let CC = "u";
+//console.log(typeof (CC)); 
+//let num = Number(CC); 
+//console.log(typeof (num))
+//console.log(CC)
